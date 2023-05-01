918db0c884e8382481f37a5d14f35911d3710917
Christopher Trimboli <christopher.trimboli@protonmail.com>
2023-05-01 10:31:24 -0600

    improve webgpu renderer

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,20 @@ import WebGPURenderer from "../three/addons/jsm/renderers/webgpu/WebGPURenderer.
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import ThreeBox from "../components/ThreeBox";
+import { useState } from "react";
 
 export default function Home() {
+  const [frameloop, setFrameLoop] =
+    useState<"never" | "always" | "demand" | undefined>("never");
+
   return (
     <main>
       <Canvas
-        gl={(canvas) => new WebGPURenderer({ canvas })}
-        frameloop="never"
-        onCreated={async (state) => {
-          await (state.gl as any).init();
-          state.set({ frameloop: "always" });
+        frameloop={frameloop}
+        gl={(canvas) => {
+          const renderer = new WebGPURenderer({ canvas });
+          renderer.init().then(() => setFrameLoop("always"));
+          return renderer;
         }}
       >
         <OrbitControls />
